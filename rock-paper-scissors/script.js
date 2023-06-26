730742330f5ed954afef63004c014032defb5e58
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-06-25 23:29:50 -0400

    Add functionality to see who wins. Not using arrays, objects, or any data structures

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,8 +1,35 @@
 getComputerChoice = () => {
-    const randomNum = Math.random();
-    if (randomNum < 0.33) return 'Rock'
-    else if (randomNum < 0.66) return 'Paper'
-    else return 'Scissor'
-}
+  const randomNum = Math.random();
+  if (randomNum < 0.33) return "Rock";
+  else if (randomNum < 0.66) return "Paper";
+  else return "Scissors";
+};
 
-//console.log(getComputerChoice());
+
+playRound = (playerSelection, computerSelection) => {
+  const formatPlayerSelection =
+    playerSelection.charAt(0).toUpperCase() +
+    playerSelection.slice(1).toLowerCase();
+
+  if (formatPlayerSelection === "Rock" && computerSelection === "Scissors") {
+    return "You Win! Rock beats Scissors";
+  } else if (formatPlayerSelection === "Rock" && computerSelection === "Paper") {
+    return "You Lose! Paper beats Rock";
+  } else if (formatPlayerSelection === "Paper" && computerSelection === "Rock") {
+    return "You Win! Paper beats Rock";
+  } else if (formatPlayerSelection === "Paper" && computerSelection === "Scissors") {
+    return "You Lose! Scissors beats Paper";
+  } else if (formatPlayerSelection === "Scissors" && computerSelection === "Paper") {
+    return "You Win! Scissors beats Paper";
+  } else if (formatPlayerSelection === "Scissors" && computerSelection === "Rock") {
+    return "You Lose! Rock beats Scissors";
+  } else if (formatPlayerSelection === computerSelection) {
+    return "It's a draw!"
+  }
+};
+
+const playerSelection = "paper";
+console.log(`Player Selection: ${playerSelection}`);
+const computerSelection = getComputerChoice();
+console.log(`Computer Selection: ${computerSelection}`);
+console.log(playRound(playerSelection, computerSelection));
